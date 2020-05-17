f75ddfbcda7b2e9e08c3596079fb0494e0d56a71
Neville Ryant <nryant@gmail.com>
2020-05-17 14:39:34 -0400

    MAINT: Update hard-coded paths.

--- extract_w2v_feats.sh
+++ extract_w2v_feats.sh
@@ -4,12 +4,12 @@
 source ../venv/bin/activate
 
 # Paths to wav2vec/vq-wav2vec model files.
-W2V_MODELF=wav2vec/models/wav2vec_large.pt
+W2V_MODELF=../wav2vec/models/wav2vec_large.pt
 W2V_FEATS_DIR=feats/wav2vec-large
-VQW2V_MODELF=wav2vec/models/vq-wav2vec_kmeans.pt
+VQW2V_MODELF=../wav2vec/models/vq-wav2vec_kmeans.pt
 VQW2V_FEATS_DIR=feats/vq-wav2vec_kmeans_roberta
-ROBERTA_MODELF=wav2vec/models/roberta/bert_kmeans.pt
-ROBERTA_VOCABF=wav2vec/models/roberta/dict.txt
+ROBERTA_MODELF=../wav2vec/models/roberta/bert_kmeans.pt
+ROBERTA_VOCABF=../wav2vec/models/roberta/dict.txt
 
 for corpus in ctimit_final  ffmtimit_final  ntimit_final  stctimit_final  timit_final  wtimit_final; do
     wav_dir=/data/corpora/processed_timit_variants/${corpus}/wav
